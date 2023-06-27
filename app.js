b141706fff4b50e6f449d7dae625c152a8ca4022
Alvarado08 <jorgealvarado0238@gmail.com>
2023-06-26 23:31:17 -0500

    Power Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const libraryEl = document.querySelector("#library");
+let library = [];
+
+// Book Constructor
+function Book(title,author,published,pages,status){
+    this.title = title;
+    this.author = author;
+    this.published = published;
+    this.pages = pages;
+    this.status = status;
+}
+
+// Adding books to library array
+function addBook(book){
+    library.push(book);
+}
+
+// Making book card
+function bookCard(book){
+    const bookCardEl = document.createElement("div");
+    bookCardEl.classList.add('shadow', 'rounded', 'bg-white', 'p-5');
+    bookCardEl.innerHTML = `
+            <h3 class="text-3xl font-bold text-center">${book.title}</h3>
+            <h3 class="text-xl font-bold">Author: <span class="text-lg font-semibold">${book.author}</span></h3>
+            <h3 class="text-xl font-bold">Published: <span class="text-lg font-semibold">${book.published}</span></h3>
+            <h3 class="text-xl font-bold">Pages: <span class="text-lg font-semibold">${book.pages}</span></h3>
+            <h3 class="text-xl font-bold">Status: <span class="text-lg font-semibold ${book.status === true ? "text-green-500" : "text-yellow-600"}">${book.status === true ? "Read" : "Reading"}</span></h3>
+    `
+    libraryEl.appendChild(bookCardEl);
+}
+
+// Showing book cards
+function showLibrary(){
+    library.forEach(book => {
+        bookCard(book);
+    })
+}
+
+addBook(new Book("John's Adventure","Jane Wayne",1995,200,false));
+addBook(new Book("Jane's Adventure","John Wayne",1990,500,true));
+addBook(new Book("Become A Millionare","Chuck Norris",2021,1000,true));
+showLibrary();
